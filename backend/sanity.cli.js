473e555d44d910868be6e1cfad66e8ca59d9e13d
slyxh2 <750772121@qq.com>
2023-02-07 01:05:15 -0500

    backend setup

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '06fwndg0',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/index.js
@@ -0,0 +1,12 @@
+import testimonials from "./testimonials"
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+export const schemaTypes = [testimonials, works, brands, abouts, experiences, skills, workExperience, contact];
