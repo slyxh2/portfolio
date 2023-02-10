4f78e02aa0982148c2643fa57403f4a7ab59f866
slyxh2 <750772121@qq.com>
2023-02-09 19:13:49 -0500

    images and Navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import './Navbar.scss';
+import { images } from '../../constants';
+import { useState } from 'react';
+
+const navItems = ['home', 'about', 'work', 'skills', 'contact',]
+const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+    const handleNavMenu = () => {
+        setToggle(!toggle);
+    }
+    return <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+            <img src={images.logo} alt="logo" />
+        </div>
+        <ul className='app__navbar-links'>
+            {navItems.map((item, index) => <li key={index} className='app__flex p-text'>
+                <div></div>
+                <a href={`#${item}`}>{item}</a>
+            </li>)}
+        </ul>
+        <div className='app__navbar-menu'>
+            <HiMenuAlt4 onClick={handleNavMenu} />
+            {toggle && (
+                <motion.div
+                    whileInView={{ x: [300, 0] }}
+                    transition={{ duration: 0.85, ease: 'easeOut' }}
+                >
+                    <HiX onClick={() => setToggle(false)} />
+                    <ul>
+                        {navItems.map((item) => (
+                            <li key={item}>
+                                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                                    {item}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </motion.div>
+            )}
+        </div>
+    </nav>
+}
+export default Navbar;
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,72 @@
+import email from '../static/img/email.png';
+import mobile from '../static/img/mobile.png';
+import api from '../static/img/api.png';
+import cpp from '../static/img/cpp.png';
+import css from '../static/img/css.png';
+import figma from '../static/img/figma.png';
+import flutter from '../static/img/flutter.png';
+import git from '../static/img/git.png';
+import graphql from '../static/img/graphql.png';
+import html from '../static/img/html.png';
+import javascript from '../static/img/javascript.png';
+import mu5 from '../static/img/mu5.png';
+import node from '../static/img/node.png';
+import python from '../static/img/python.png';
+import react from '../static/img/react.png';
+import redux from '../static/img/redux.png';
+import sass from '../static/img/sass.png';
+import typescript from '../static/img/typescript.png';
+import vue from '../static/img/vue.png';
+
+import about01 from '../static/img/about01.png';
+import about02 from '../static/img/about02.png';
+import about03 from '../static/img/about03.png';
+import about04 from '../static/img/about04.png';
+
+import profile from '../static/img/profile.png';
+import circle from '../static/img/circle.svg';
+import logo from '../static/img/logo.png';
+
+import adidas from '../static/img/adidas.png';
+import amazon from '../static/img/amazon.png';
+import asus from '../static/img/asus.png';
+import bolt from '../static/img/bolt.png';
+import nb from '../static/img/nb.png';
+import skype from '../static/img/skype.png';
+import spotify from '../static/img/spotify.png';
+
+export default {
+    email,
+    mobile,
+    api,
+    cpp,
+    css,
+    figma,
+    flutter,
+    git,
+    graphql,
+    html,
+    javascript,
+    mu5,
+    node,
+    python,
+    react,
+    redux,
+    sass,
+    typescript,
+    vue,
+    about01,
+    about02,
+    about03,
+    about04,
+    profile,
+    circle,
+    logo,
+    adidas,
+    amazon,
+    asus,
+    bolt,
+    nb,
+    skype,
+    spotify,
+};
